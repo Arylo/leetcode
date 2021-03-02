155f7a3188159e55c7adf831b5e27c73c6a76b1f
doubleyyang <doubleyyang@tencent.com>
2021-03-02 12:42:30 +0800

    feat: add #304 daily

---
+++ #304/index.spec.ts
@@ -0,0 +1,27 @@
+import { startTest } from '../utils';
+
+const matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]
+]
+
+const fnName = 'sumRegion';
+const units: Array<[any[], any]> = [
+  [
+    [matrix, 2, 1, 4, 3],
+    8,
+  ],
+  [
+    [matrix, 1, 1, 2, 2],
+    11,
+  ],
+  [
+    [matrix, 1, 2, 2, 4],
+    12,
+  ],
+];
+
+startTest(__dirname, fnName, units);
---
+++ #304/test1.ts
@@ -0,0 +1,20 @@
+export function sumRegion(matrix: number[][], row1: number, col1: number, row2: number, col2: number) {
+  return new NumMatrix(matrix).sumRegion(row1, col1, row2, col2);
+}
+
+class NumMatrix {
+  private matrix: number[][] = [];
+  constructor(matrix: number[][]) {
+    this.matrix = matrix;
+  }
+
+  sumRegion(row1: number, col1: number, row2: number, col2: number): number {
+    let total = 0;
+    for (let i = row1; i < row2 + 1; i++) {
+      for (let j = col1; j < col2 + 1; j++) {
+        total += this.matrix[i][j];
+      }
+    }
+    return total;
+  }
+}
